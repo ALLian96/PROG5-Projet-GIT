60eda70fa9fcfb4da91d432f4be1215b351313e5
OOPotato <45450168+OOPotato@users.noreply.github.com>
2019-01-10 10:59:53 +0100

    OP

---
+++ LanceProgramme.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+choix="r"
+rep="Examples_loader"
+cd "$rep"
+drp=0
+
+while [ $choix = "r" ]
+do
+	for file in *.o
+			do  
+
+				   echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*
+				   echo \*\*         $file          \*\* 
+				   echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*
+				   echo ""
+					
+			done
+
+		echo Donnez un fichier test ELF :
+		read -r fic 
+
+
+			for file in *
+			do
+					if [ $file = $fic ]
+					then
+						drp=1
+							
+					fi
+			done
+
+			if [ $drp -eq 1 ]
+			then
+			  cd ..
+			  make
+			  chmod +x read_elf
+			  ./read_elf "Examples_loader/$fic"
+			else 
+				echo Le fichier \< $fic \> n\'existe pas parmi les fichiers de test
+			fi
+
+		  echo Voulez vous recommencer avec un autre exemple ? \<====\>  r
+		  echo Quitter                                        \<====\>  q
+		  read -r rp
+          
+          if [ $rp = "r" ]
+          then
+          	clear
+          	cd "$rep"
+          fi 
+
+		  choix=$rp
+done
