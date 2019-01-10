a2a8ee7c3de86d6fd811a029bce9de1623af3f0b
HafidHM <45362111+HafidHM@users.noreply.github.com>
2019-01-10 10:35:11 +0000

    Update LanceProgramme.sh

--- LanceProgramme.sh
+++ LanceProgramme.sh
@@ -5,50 +5,56 @@ rep="Examples_loader"
 cd "$rep"
 drp=0
 
-while [ $choix = "r" ]
-do
-	for file in *.o
-			do  
+		while [ $choix = "r" ]
+		do
+			for file in *.o
+					do  
 
-				   echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*
-				   echo \*\*         $file          \*\* 
-				   echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*
-				   echo ""
-					
-			done
-
-		echo Donnez un fichier test ELF :
-		read -r fic 
-
-
-			for file in *
-			do
-					if [ $file = $fic ]
-					then
-						drp=1
+						   echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*
+						   echo \*\*         $file          \*\* 
+						   echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*
+						   echo ""
 							
+					done
+
+				echo Donnez svp un fichier test ELF :
+				read -r fic 
+
+				echo Donnez svp un fichier de sortie :
+				read -r out 
+
+                touch out 
+
+					for file in *
+					do
+							if [ $file = $fic ]
+							then
+								drp=1
+									
+							fi
+					done
+
+					if [ $drp -eq 1 ]
+					then
+					  cd ..
+					  make
+					  chmod +x read_elf
+					  ./read_elf "Examples_loader/$fic" "Examples_loader/$out"
+					else 
+						echo Le fichier \< $fic \> n\'existe pas parmi les fichiers de test
 					fi
-			done
 
-			if [ $drp -eq 1 ]
-			then
-			  cd ..
-			  make
-			  chmod +x read_elf
-			  ./read_elf "Examples_loader/$fic"
-			else 
-				echo Le fichier \< $fic \> n\'existe pas parmi les fichiers de test
-			fi
+				  echo Vous voulez recommancer avec un autre example  \<====\>  r
+				  echo Quitter                                        \<====\>  q
+				  read -r rp
+		          
+		          if [ $rp = "r" ]
+		          then
+		          	clear
+		          	cd "$rep"
+		          fi 
 
-		  echo Voulez vous recommencer avec un autre exemple ? \<====\>  r
-		  echo Quitter                                        \<====\>  q
-		  read -r rp
-          
-          if [ $rp = "r" ]
-          then
-          	clear
-          	cd "$rep"
-          fi 
+				  choix=$rp
+		done
 
-		  choix=$rp
-done
+			
